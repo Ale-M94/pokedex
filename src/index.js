2d29b457212ac15b2f1a77842c0ae4ca1cccdf2c
Ale-M94 <175752593+Ale-M94@users.noreply.github.com>
2024-12-12 17:17:12 -0300

    Genera lista de pokemones
    Permite obtener lista de 20 pokemones por medio de una función y agrega botones "siguiente" y "anterior"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-const pokeApi = fetch('https://pokeapi.co/api/v2/pokemon');
+const pokeApi = 'https://pokeapi.co/api/v2/pokemon/';
 const tablaPokemones = document.querySelector('#lista-pokemones');
+const $siguiente = document.querySelector('#boton-siguiente');
+const $anterior = document.querySelector('#boton-anterior');
 
+let paginaActual = 1;
+const limitePorPagina = 20;
 
-pokeApi
+function obtenerListaPokemon(url){
+    fetch(url)
     .then(response => response.json())
     .then(responseJSON => {
+        tablaPokemones.innerHTML = '';
+
         responseJSON.results.forEach((pokemon, index) =>{
-            const numeroPokemon = index + 1;
+            const numeroPokemon = (paginaActual - 1) * limitePorPagina + (index + 1);
             const fila = document.createElement('tr');
             const celdaNumero = document.createElement('td');
             const celdaNombre = document.createElement('td');
@@ -18,6 +25,36 @@ pokeApi
             fila.appendChild(celdaNombre);
 
             tablaPokemones.appendChild(fila);
-        })
+        });
+
+        if (responseJSON.next) {
+            $siguiente.style.visibility = 'visible';
+            $siguiente.onclick = function () {
+                paginaActual++;
+                obtenerListaPokemon(responseJSON.next);
+            };
+        } else {
+            $siguiente.style.visibility = 'hidden';
+        };
+
+        if (responseJSON.previous) {
+            $anterior.style.visibility = 'visible';
+            $anterior.onclick = function () {
+                paginaActual--;
+                obtenerListaPokemon(responseJSON.previous);
+            };
+        } else {
+            $anterior.style.visibility = 'hidden';
+        }
+
     })
+
+    .catch(error => console.error('No se pudo obtener la lista de Pokemones', error));
+}
+
+
+
+
+obtenerListaPokemon(pokeApi);
+
 
