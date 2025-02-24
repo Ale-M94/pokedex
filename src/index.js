08be3915fe7123592bfc2e9d6f7c5168c6dae5f3
Ale-M94 <175752593+Ale-M94@users.noreply.github.com>
2025-02-24 11:44:59 -0300

    Crea interfaz de listado con solicitud a la api
    Primeras funciones para separar en módulos la pokedex

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
-import {
-    obtenerListaPokemon,
-    buscarPorNombre,
-    buscarPorNumero,
-} from './ui.js';
-import { pokeApi } from './pokedex.js';
+// import {
+//     obtenerListaPokemon,
+//     buscarPorNombre,
+//     buscarPorNumero,
+// } from './ui.js';
+// import { pokeApi } from './pokedex.js';
 
-function inicializar() {
-    obtenerListaPokemon(pokeApi);
-    buscarPorNombre();
-    buscarPorNumero();
+function iniciar() {
+    obtenerPokemones();
 };
 
-inicializar();
+iniciar();
+
+function mostrarTotalPokemones(totalPokemones) {
+    document.querySelector('#total-pokemones').textContent = totalPokemones;
+};
+
+function obtenerPokemones() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/`).then((r) => r.json()).then((r) => {
+        console.log(r);
+        const { count: totalPokemones, results: pokemones } = r;
+        mostrarTotalPokemones(totalPokemones);
+        mostrarListadoPokemones(pokemones);
+    })
+};
+
+function mostrarListadoPokemones(pokemones) {
+    pokemones.forEach((pokemon) => {
+        const { name: nombre, url, sprites: imagen } = pokemon;
+        const $listado = document.querySelector('#listado-pokemones');
+        // const $imagenPokemon = document.querySelector('#imagen-pokemon');
+
+        const $link = document.createElement('a');
+        $link.className = "list-group-item list-group-item-action";
+        $link.setAttribute('href', '#');
+        $link.textContent = nombre;
+        $link.addEventListener('click', () => {
+            // trabajar con async await            
+            console.log(`buscar data en ${url}`)
+        })
+
+        $listado.appendChild($link);
+    });
+};
+
+function mostrarCartaPokemon(){
+    
+}
--- src/ui.js
+++ src/ui.js
@@ -1,130 +1,131 @@
-const $siguiente = document.querySelector('#boton-siguiente');
-const $anterior = document.querySelector('#boton-anterior');
-const tarjetaPokemones = document.querySelector('#tarjeta-pokemon');
+// const $siguiente = document.querySelector('#boton-siguiente');
+// const $anterior = document.querySelector('#boton-anterior');
+// const tarjetaPokemones = document.querySelector('#tarjeta-pokemon');
 
-let paginaActual = 1;
-const limitePorPagina = 20;
+// let paginaActual = 1;
+// const limitePorPagina = 20;
 
-export function obtenerListaPokemon(url) {
-    const listaPokemones = document.querySelector('#lista-pokemones');
+// export function obtenerListaPokemon(url) {
+//     const listaPokemones = document.querySelector('#lista-pokemones');
 
-    fetch(url)
-        .then(response => response.json())
-        .then(responseJSON => {
-            listaPokemones.innerHTML = '';
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(responseJSON => {
+//             listaPokemones.innerHTML = '';
 
-            responseJSON.results.forEach((pokemon, index) => {
-                const numeroPokemon = (paginaActual - 1) * limitePorPagina + (index + 1);
-                const fila = document.createElement('tr');
-                const celdaNumero = document.createElement('td');
-                const celdaNombre = document.createElement('td');
+//             responseJSON.results.forEach((pokemon, index) => {
+//                 const numeroPokemon = (paginaActual - 1) * limitePorPagina + (index + 1);
+//                 const fila = document.createElement('tr');
+//                 const celdaNumero = document.createElement('td');
+//                 const celdaNombre = document.createElement('td');
+//                 const linkPokemon = document.createElem
 
-                celdaNumero.textContent = numeroPokemon;
-                fila.appendChild(celdaNumero);
+//                 celdaNumero.textContent = numeroPokemon;
+//                 fila.appendChild(celdaNumero);
 
-                celdaNombre.textContent = pokemon.name;
-                fila.appendChild(celdaNombre);
+//                 celdaNombre.textContent = pokemon.name;
+//                 fila.appendChild(celdaNombre);
 
-                fila.value = pokemon.url;
+//                 fila.value = pokemon.url;
 
-                listaPokemones.appendChild(fila);
-            });
+//                 listaPokemones.appendChild(fila);
+//             });
 
-            if (responseJSON.next) {
-                $siguiente.classList.remove('d-none');
-                $siguiente.onclick = function () {
-                    paginaActual++;
-                    obtenerListaPokemon(responseJSON.next);
-                };
-            } else {
-                $siguiente.classList.add('d-none');
-            };
+//             if (responseJSON.next) {
+//                 $siguiente.classList.remove('d-none');
+//                 $siguiente.onclick = function () {
+//                     paginaActual++;
+//                     obtenerListaPokemon(responseJSON.next);
+//                 };
+//             } else {
+//                 $siguiente.classList.add('d-none');
+//             };
 
-            if (responseJSON.previous) {
-                $anterior.classList.remove('d-none');
+//             if (responseJSON.previous) {
+//                 $anterior.classList.remove('d-none');
 
-                $anterior.onclick = function () {
-                    paginaActual--;
-                    obtenerListaPokemon(responseJSON.previous);
-                };
-            } else {
-                $anterior.classList.add('d-none');
-            }
+//                 $anterior.onclick = function () {
+//                     paginaActual--;
+//                     obtenerListaPokemon(responseJSON.previous);
+//                 };
+//             } else {
+//                 $anterior.classList.add('d-none');
+//             }
 
-        })
+//         })
 
-        .catch(error => console.error('No se pudo obtener la lista de Pokemones', error));
-};
+//         .catch(error => console.error('No se pudo obtener la lista de Pokemones', error));
+// };
 
-export function buscarPorNombre() {
-    const valorNombre = document.querySelector('#nombre-pokemon');
-    const $buscarPorNombre = document.querySelector('#boton-buscar-nombre');
+// export function buscarPorNombre() {
+//     const valorNombre = document.querySelector('#nombre-pokemon');
+//     const $buscarPorNombre = document.querySelector('#boton-buscar-nombre');
 
-    $buscarPorNombre.onclick = function (event) {
-        event.preventDefault;
+//     $buscarPorNombre.onclick = function (event) {
+//         event.preventDefault;
 
-        document.querySelector('#tabla-pokemones').classList.add('d-none');
-        $anterior.classList.add('d-none');
-        $siguiente.classList.add('d-none');
+//         document.querySelector('#tabla-pokemones').classList.add('d-none');
+//         $anterior.classList.add('d-none');
+//         $siguiente.classList.add('d-none');
 
-        if (valorNombre.value === '') {
-            alert('Ingrese un nombre');
-            return;
-        };
+//         if (valorNombre.value === '') {
+//             alert('Ingrese un nombre');
+//             return;
+//         };
 
-        let nombreCorregido = valorNombre.value.toLowerCase()
+//         let nombreCorregido = valorNombre.value.toLowerCase()
 
 
-        fetch(`https://pokeapi.co/api/v2/pokemon/${nombreCorregido}`)
-            .then(response => response.json())
-            .then(responseJSON => {
-                tarjetaPokemones.classList.remove('d-none')
-                const tipos = responseJSON.types.map(tipos => tipos.type.name)
+//         fetch(`https://pokeapi.co/api/v2/pokemon/${nombreCorregido}`)
+//             .then(response => response.json())
+//             .then(responseJSON => {
+//                 tarjetaPokemones.classList.remove('d-none')
+//                 const tipos = responseJSON.types.map(tipos => tipos.type.name)
 
-                document.querySelector('.card-title').innerHTML = responseJSON.species.name
-                document.querySelector('.card-text').innerHTML = `Número: ${responseJSON.id}<br>
-                Tipos: ${tipos.join(', ')}<br>
-                Altura: ${responseJSON.height}<br>
-                Peso: ${responseJSON.weight}`
-                document.querySelector('#imagen-pokemon').src = responseJSON.sprites.front_default
+//                 document.querySelector('.card-title').innerHTML = responseJSON.species.name
+//                 document.querySelector('.card-text').innerHTML = `Número: ${responseJSON.id}<br>
+//                 Tipos: ${tipos.join(', ')}<br>
+//                 Altura: ${responseJSON.height}<br>
+//                 Peso: ${responseJSON.weight}`
+//                 document.querySelector('#imagen-pokemon').src = responseJSON.sprites.front_default
 
-            })
+//             })
 
-            .catch(error => console.error('El nombre ingresado no es válido', error))
-    };
-};
+//             .catch(error => console.error('El nombre ingresado no es válido', error))
+//     };
+// };
 
-export function buscarPorNumero() {
-    const valorNumero = document.querySelector('#numero-pokemon');
-    const $buscarPorNumero = document.querySelector('#boton-buscar-numero');
+// export function buscarPorNumero() {
+//     const valorNumero = document.querySelector('#numero-pokemon');
+//     const $buscarPorNumero = document.querySelector('#boton-buscar-numero');
 
-    $buscarPorNumero.onclick = function (event) {
-        event.preventDefault;
+//     $buscarPorNumero.onclick = function (event) {
+//         event.preventDefault;
 
-        document.querySelector('#tabla-pokemones').classList.add('d-none');
-        $anterior.classList.add('d-none');
-        $siguiente.classList.add('d-none');
+//         document.querySelector('#tabla-pokemones').classList.add('d-none');
+//         $anterior.classList.add('d-none');
+//         $siguiente.classList.add('d-none');
 
-        if (valorNumero.value === '') {
-            alert('Ingrese un nombre');
-            return;
-        };
+//         if (valorNumero.value === '') {
+//             alert('Ingrese un nombre');
+//             return;
+//         };
 
-        fetch(`https://pokeapi.co/api/v2/pokemon/${valorNumero.value}`)
-            .then(response => response.json())
-            .then(responseJSON => {
-                tarjetaPokemones.classList.remove('d-none')
-                const tipos = responseJSON.types.map(tipos => tipos.type.name)
+//         fetch(`https://pokeapi.co/api/v2/pokemon/${valorNumero.value}`)
+//             .then(response => response.json())
+//             .then(responseJSON => {
+//                 tarjetaPokemones.classList.remove('d-none')
+//                 const tipos = responseJSON.types.map(tipos => tipos.type.name)
 
-                document.querySelector('.card-title').innerHTML = responseJSON.species.name
-                document.querySelector('.card-text').innerHTML = `Número: ${responseJSON.id}<br>
-        Tipos: ${tipos.join(', ')}<br>
-        Altura: ${responseJSON.height}<br>
-        Peso: ${responseJSON.weight}`
-                document.querySelector('#imagen-pokemon').src = responseJSON.sprites.front_default
+//                 document.querySelector('.card-title').innerHTML = responseJSON.species.name
+//                 document.querySelector('.card-text').innerHTML = `Número: ${responseJSON.id}<br>
+//         Tipos: ${tipos.join(', ')}<br>
+//         Altura: ${responseJSON.height}<br>
+//         Peso: ${responseJSON.weight}`
+//                 document.querySelector('#imagen-pokemon').src = responseJSON.sprites.front_default
 
-            })
+//             })
 
-            .catch(error => console.error('El número ingresado no es válido', error))
-    };
-};
+//             .catch(error => console.error('El número ingresado no es válido', error))
+//     };
+// };
